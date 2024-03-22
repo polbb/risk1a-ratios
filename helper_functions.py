e5fe3631f2b8aadb90eb681c092914f3e13610d7
polbb <paulbessone@gmail.com>
2024-03-22 08:48:24 +0000

    first commit

--- helper_functions.py
+++ helper_functions.py
@@ -12,11 +12,17 @@ def make_dataframe(results):
 def display_metrics(name1, ratio1, name2, ratio2):
     col1, col2 = st.columns([3,3])
 
-    col1.metric(name1, f"{round(ratio1, 2)}")
-    col2.metric(name2, f"{round(ratio2, 2)}")
+    ratio1_display = f"{round(ratio1, 2)}" if isinstance(ratio1, (int, float)) else ratio1
+    ratio2_display = f"{round(ratio2, 2)}" if isinstance(ratio2, (int, float)) else ratio2
+
+    col1.metric(name1, ratio1_display)
+    col2.metric(name2, ratio2_display)
 
 def display_metrics_floor(name1, ratio1, name2, ratio2):
     col1, col2 = st.columns([3,3])
 
-    col1.metric(name1, f"{round(ratio1, 0)}")
-    col2.metric(name2, f"{round(ratio2, 0)}")
+    ratio1_display = f"{round(ratio1, 0)}" if isinstance(ratio1, (int, float)) else ratio1
+    ratio2_display = f"{round(ratio2, 0)}" if isinstance(ratio2, (int, float)) else ratio2
+
+    col1.metric(name1, ratio1_display)
+    col2.metric(name2, ratio2_display)
